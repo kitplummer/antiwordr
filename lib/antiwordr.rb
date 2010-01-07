21512e1e3faca0435d7f24d8869dff32475444ad
Kit Plummer <kplummer@blitz.(none)>
2010-01-07 10:21:14 -0700

    initial drop.

---
+++ lib/antiwordr.rb
@@ -0,0 +1,96 @@
+# The library has a single method for converting PDF files into HTML. The
+# method current takes in the source path, and either/both the user and owner
+# passwords set on the source PDF document.  The convert method returns the
+# HTML as a string for further manipulation of loading into a Document.
+#
+# Requires that pdftohtml be installed and on the path
+#
+# Author:: Kit Plummer (mailto:kitplummer@gmail.com)
+# Copyright:: Copyright (c) 2010 Kit Plummer
+# License:: MIT
+
+require 'rubygems'
+require 'nokogiri'
+require 'uri'
+require 'open-uri'
+require 'tempfile'
+
+module AntiWordR
+  
+  # Simple local error abstraction
+  class AntiWordRError < RuntimeError; end
+  
+  VERSION = '0.1.0'
+
+  # Provides facilities for converting Word Docs to Text rom Ruby code.
+  class DocFile
+    attr :path
+    attr :target
+    attr :format
+    
+    def initialize(input_path, target_path=nil)
+      @path = input_path
+      @target = target_path
+    end
+
+    # Convert the PDF document to HTML.  Returns a string
+    def convert()
+      errors = ""
+      output = ""
+      
+      cmd = "antiword #{format}" + ' "' + @path + '"'
+      
+      output = `#{cmd} 2>&1`
+      
+      if (output.include?("command not found"))
+        raise AntiWordRError, "AntiWordR requires antiword to be installed"
+      elsif (output.include?("is not a Word Document"))
+        raise AntiWordRError, "#{@path} is not a Word Document"
+      elsif (output.include?("Error:"))
+        raise AntiWordRError, output.split("\n").first.to_s.chomp
+      else
+        return output
+      end
+    end
+    
+    # Convert the PDF document to HTML.  Returns a Nokogiri::HTML:Document
+    def convert_to_docbook_document() 
+      Nokogiri::XML.parse(convert_to_docbook())
+    end
+    
+    def convert_to_docbook()
+      @format = "-x db"
+      convert()
+    end
+  end
+  
+  # Handle a string-based local path as input, extends PdfFile
+  class DocFilePath < DocFile
+    def initialize(input_path, target_path=nil)
+      # check to make sure file is legit
+      if (!File.exist?(input_path))
+        raise AntiWordRError, "invalid file path"
+      end
+      
+      super(input_path, target_path)
+      
+    end 
+  end
+  
+  # Handle a URI as a remote path to a PDF, extends PdfFile
+  class DocFileUrl < DocFile
+    def initialize(input_url, target_path=nil)
+      # check to make sure file is legit
+      begin
+        if ((input_url =~ URI::regexp).nil?)
+          raise AntiWordRError, "invalid file url"
+        end
+        tempfile = Tempfile.new('antiwordr')
+        File.open(tempfile.path, 'wb') {|f| f.write(open(input_url).read) }
+        super(tempfile.path, target_path)
+      rescue => bang
+        raise AntiWordRError, bang.to_s
+      end
+    end
+  end
+end
---
+++ test/antiwordr_test.rb
@@ -0,0 +1,86 @@
+require 'test/unit'
+require File.join(File.dirname(__FILE__), '../lib/antiwordr')
+
+class DocFileTest < Test::Unit::TestCase
+  include AntiWordR
+
+  CURRENT_DIR = File.dirname(File.expand_path(__FILE__)) + "/"
+  TEST_DOC_PATH = CURRENT_DIR + "test.doc"
+  TEST_BAD_PATH = "blah.doc"
+  TEST_NON_DOC = CURRENT_DIR + "antiwordr_test.rb"
+  TEST_URL_DOC =
+   "http://github.com/kitplummer/antiwordr/raw/master/test/test.doc"
+  TEST_URL_NON_DOC =
+   "http://github.com/kitplummer/antiwordr/raw/master/test/antiwordr_test.rb"
+  
+  def test_docfile_new
+    file = DocFilePath.new(TEST_DOC_PATH, ".")
+    assert file
+  end
+  
+  def test_invalid_docfile
+    e = assert_raise AntiWordRError do 
+      file = DocFilePath.new(TEST_NON_DOC, ".")
+      file.convert
+    end
+    assert_equal "#{TEST_NON_DOC} is not a Word Document", e.to_s
+  end
+
+  def test_bad_docfile_new
+    e = assert_raise AntiWordRError do
+      file = DocFilePath.new(TEST_BAD_PATH, ".")
+    end
+    assert_equal "invalid file path", e.to_s
+  end
+
+  def test_string_from_docfile
+    file = DocFilePath.new(TEST_DOC_PATH, ".")
+    assert_equal "String", file.convert().class.to_s
+    assert_equal `antiword "#{TEST_DOC_PATH}"`, file.convert() 
+  end 
+
+  def test_return_docbook
+    file = DocFilePath.new(TEST_DOC_PATH, ".")
+    assert_equal "String", file.convert_to_docbook().class.to_s
+  end
+
+  def test_return_docbook_document
+    file = DocFilePath.new(TEST_DOC_PATH, ".")
+    assert_equal "Nokogiri::XML::Document",
+    file.convert_to_docbook_document().class.to_s
+    assert_equal Nokogiri::XML.parse(
+    `antiword -x db "#{TEST_DOC_PATH}"`
+    ).css('para').first.to_s,
+    file.convert_to_docbook_document().css('para').first.to_s
+  end
+
+  #   def test_invalid_URL_pdffile
+  #     e = assert_raise AntiWordRError do
+  #       file = DocFileUrl.new("blah", ".", nil, nil)
+  #     end
+  #     assert_equal "invalid file url", e.to_s
+  #   end
+  #   
+  #   def test_invalid_URL_resource_pdffile
+  #     e = assert_raise AntiWordRError do
+  #       file = DocFileUrl.new("http://github.com/kitplummer/blah", ".", nil, nil)
+  #     end
+  #     assert_equal "404 Not Found", e.to_s
+  #   end
+  #   
+  #   def test_invalid_URL_pdf_pdffile
+  #     e = assert_raise AntiWordRError do
+  #       file = DocFileUrl.new(TEST_URL_NON_DOC, ".", nil, nil)
+  #       file.convert
+  #     end
+  #     assert_equal "Error: May not be a DOC file (continuing anyway)", e.to_s
+  #   end
+  #   
+  #   def test_valid_URL_pdffile
+  #     # http://github.com/kitplummer/pdftohtmlr/raw/master/test/test.pdf
+  #     file = DocFileUrl.new(TEST_URL_DOC, ".", nil, nil)
+  #     assert_equal "String", file.convert().class.to_s
+  #     assert_equal `pdftohtml -stdout "#{TEST_DOC_PATH}"`, file.convert()
+  #   end
+
+end
